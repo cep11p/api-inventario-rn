367f7fc8927ae59a0d787960fb061117d3699e90
cep11p <cep11p@gmail.com>
2021-10-20 13:49:18 -0300

    TG-391 - TG-392 #Closed Se configura el control de acceso en cada controlador https://tree.taiga.io/project/areangonzalez-gestor-de-inventario/us/391

--- modules/api/controllers/LocalidadController.php
+++ modules/api/controllers/LocalidadController.php
@@ -28,24 +28,18 @@ class LocalidadController extends ActiveController{
 
         $behaviors['authenticator'] = $auth;
 
-//        $behaviors['authenticator'] = [
-//            'class' => \yii\filters\auth\HttpBearerAuth::className(),
-//        ];
+        $behaviors['authenticator'] = [
+            'class' => \yii\filters\auth\HttpBearerAuth::className(),
+        ];
 
         // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
         $behaviors['authenticator']['except'] = ['options'];     
 
-//        $behaviors['access'] = [
-//            'class' => \yii\filters\AccessControl::className(),
-//            'only' => ['index'],
-//            'rules' => [
-//                [
-//                    'allow' => true,
-//                    'actions' => ['index'],
-//                    'roles' => ['consultar_persona'],
-//                ],
-//            ]
-//        ];
+        $behaviors['access'] = [
+            'class' => \yii\filters\AccessControl::className(),
+            'only' => ['@'],
+            'rules' => []
+        ];
 
 
 

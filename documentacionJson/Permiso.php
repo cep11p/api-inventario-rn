9cb96365a0665bff8defd6a29487ae1652f9a621
cep11p <cep11p@gmail.com>
2021-11-11 12:28:07 -0300

    TG-441 - TG-444 #Closed https://tree.taiga.io/project/areangonzalez-gestor-de-inventario/us/441

---
+++ documentacionJson/Permiso.php
@@ -0,0 +1,30 @@
+<?php
+
+/**** Para mostrar listado ****/
+/**
+* @url http://api.inventario.local/api/permisos
+* @method GET
+* @arrayReturn
+* 
+[
+    {
+        "name": "persona_crear",
+        "type": 2,
+        "description": "Permite registrar una persona",
+        "rule_name": null,
+        "data": null,
+        "created_at": 1609162941,
+        "updated_at": null
+    },
+    {...},
+    {
+        "name": "persona_modificar",
+        "type": 2,
+        "description": "Permite modificar una persona",
+        "rule_name": null,
+        "data": null,
+        "created_at": 1609162941,
+        "updated_at": null
+    },
+]
+*/

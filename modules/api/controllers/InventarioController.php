d356a7d2224328364915b8e070ad5f26ab8def34
cep11p <cep11p@gmail.com>
2021-11-19 12:11:40 -0300

    TG-567 Se configura el permiso defectuoso_set https://tree.taiga.io/project/areangonzalez-gestor-de-inventario/us/467

--- modules/api/controllers/InventarioController.php
+++ modules/api/controllers/InventarioController.php
@@ -70,36 +70,41 @@ class InventarioController extends ActiveController{
     /**
      * Esta accion permite realizar modificacion de ingresos. Se puede modificar dentro de 2 horas
      *
-     * @return void
+    //  * @return void
      */
-    public function actionUpdate($id){
-        $param = Yii::$app->request->post();
-        $param['comprobanteid'] = $id;
+    // public function actionUpdate($id){
+    //     $param = Yii::$app->request->post();
+    //     $param['comprobanteid'] = $id;
         
-        $transaction = Yii::$app->db->beginTransaction();
-        try {
+    //     $transaction = Yii::$app->db->beginTransaction();
+    //     try {
             
-            $model = Inventario::findOne(['comprobanteid' => $id]);
-            if($model == Null){
-                throw new Exception('El comprobante a modificar no existe.');
-            }
-            $comprobanteid = $model->modificarStock($param);
+    //         $model = Inventario::findOne(['comprobanteid' => $id]);
+    //         if($model == Null){
+    //             throw new Exception('El comprobante a modificar no existe.');
+    //         }
+    //         $comprobanteid = $model->modificarStock($param);
 
-            $transaction->commit();
+    //         $transaction->commit();
             
-            $resultado['message']='Se modifica un nuevo stock';
-            $resultado['comprobanteid']=$comprobanteid;
+    //         $resultado['message']='Se modifica un nuevo stock';
+    //         $resultado['comprobanteid']=$comprobanteid;
             
-            return  $resultado;
+    //         return  $resultado;
            
-        }catch (Exception $exc) {
-            $transaction->rollBack();
-            $mensaje =$exc->getMessage();
-            throw new \yii\web\HttpException(400, $mensaje);
-        }
-    }
+    //     }catch (Exception $exc) {
+    //         $transaction->rollBack();
+    //         $mensaje =$exc->getMessage();
+    //         throw new \yii\web\HttpException(400, $mensaje);
+    //     }
+    // }
     
      public function actionSetDefectuoso() {
+
+        if (!\Yii::$app->user->can('defectuoso_set')) {
+            throw new \yii\web\HttpException(403, 'No se tienen permisos necesarios para ejecutar esta acción');
+        }
+
         $param = \Yii::$app->request->post();
         $model = new Inventario();
         $transaction = Yii::$app->db->beginTransaction();

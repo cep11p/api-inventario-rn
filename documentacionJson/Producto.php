85c950cf3a508f971f59acde6ddcdf5731e388f6
cep11p <cep11p@gmail.com>
2021-12-27 10:41:11 -0300

    Se actualiza la documentacion de producto

--- documentacionJson/Producto.php
+++ documentacionJson/Producto.php
@@ -10,27 +10,33 @@
         "id": 1,
         "nombre": "Aceite de girasol",
         "codigo": "A300",
-        "unidad_valor": "1,5",
+        "unidad_valor": 1.5,
         "unidad_medidaid": 3,
         "marcaid": 1,
         "categoriaid": 1,
+        "activo": 1,
         "marca": "Arcor",
         "unidad_medida": "lt",
-        "producto": "Aceite de girasol, 1,5lt (Arcor)"
+        "producto": "Aceite de girasol, 1.5lt (Arcor)",
+        "categoria": "Alimento"
     },
-    {...},
     {
-        "id": 2,
-        "nombre": "Aceite de girasol",
-        "codigo": "A301",
-        "unidad_valor": "900",
-        "unidad_medidaid": 4,
-        "marcaid": 1,
+       ...
+    },
+    {
+        "id": 3,
+        "nombre": "Arroz blanco",
+        "codigo": "A302",
+        "unidad_valor": 1,
+        "unidad_medidaid": 1,
+        "marcaid": 168,
         "categoriaid": 1,
-        "marca": "Arcor",
-        "unidad_medida": "ml",
-        "producto": "Aceite de girasol, 900ml (Arcor)"
-    }
+        "activo": 1,
+        "marca": "Dos hermanos",
+        "unidad_medida": "kg",
+        "producto": "Arroz blanco, 1kg (Dos hermanos)",
+        "categoria": "Alimento"
+    },
  * ]
 */
 

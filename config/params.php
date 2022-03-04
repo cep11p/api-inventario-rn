121c46d7cce42adead1f3956d467328a7bea0623
cep11p <cep11p@gmail.com>
2022-03-04 11:10:42 -0300

    Se actualiza the secret key

--- config/params.php
+++ config/params.php
@@ -4,7 +4,7 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
-    'JWT_SECRET' => 'ZqCeBt}246',
+    'JWT_SECRET' => 'ZqCeBt}531',
     'servicioLugar'=> getenv('SERVICIO_LUGAR')?getenv('SERVICIO_LUGAR'):'app\components\DummyServicioLugar',
     'servicioRegistral'=> getenv('SERVICIO_REGISTRAL')?getenv('SERVICIO_REGISTRAL'):'app\components\DummyServicioRegistral',
     'UID_REGISTRAL'=>'4',

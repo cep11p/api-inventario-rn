d6f4e3da556ef5278291e5c07ed40001e967cb47
cep11p <cep11p@gmail.com>
2022-03-10 13:12:53 -0300

    TG-509 Se actualiza la documentacion de categoria https://tree.taiga.io/project/areangonzalez-gestor-de-inventario/us/509

--- documentacionJson/UnidadMedida.php
+++ documentacionJson/UnidadMedida.php
@@ -5,38 +5,46 @@
 * @url http://inventario.local/unidad-medidas
 * @method GET
 * @arrayReturn
-[
-    {
-        "id": 1,
-        "nombre": "Kilogramo",
-        "simbolo": "kg"
-    },
-    {
-        "id": 2,
-        "nombre": "Gramo",
-        "simbolo": "gr"
-    },
-    {
-        "id": 3,
-        "nombre": "Litro",
-        "simbolo": "lt"
-    },
-    {
-        "id": 4,
-        "nombre": "Mililitro",
-        "simbolo": "ml"
-    },
-    {
-        "id": 5,
-        "nombre": "Unidad",
-        "simbolo": "un"
-    },
-    {
-        "id": 6,
-        "nombre": "Centimetros cúbicos",
-        "simbolo": "cm3"
-    }
-]
+    [
+        {
+            "id": 1,
+            "nombre": "Kilogramo",
+            "simbolo": "kg"
+        },
+        {
+            "id": 2,
+            "nombre": "Gramo",
+            "simbolo": "gr"
+        },
+        {
+            "id": 3,
+            "nombre": "Litro",
+            "simbolo": "lt"
+        }
+    ]
+* @arrayReturn SIN PAGINACION
+{
+    "pagesize": 3,
+    "pages": 121,
+    "total_filtrado": 361,
+    "resultado": [
+        {
+            "id": 1,
+            "nombre": "Kilogramo",
+            "simbolo": "kg"
+        },
+        {
+            "id": 2,
+            "nombre": "Gramo",
+            "simbolo": "gr"
+        },
+        {
+            "id": 3,
+            "nombre": "Litro",
+            "simbolo": "lt"
+        }
+    ]
+}
 */
 
 /** Para crear
